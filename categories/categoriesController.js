1822f5710fdcdb5bb6a2adfa17aa059cbe4444a9
Carlos Welliton <carloswelliton7@gmail.com>
2024-10-19 00:28:48 -0300

    criando rota de atualização de categorias e criação de artigos com configurações do tinyMCE e diversos plugins

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -66,5 +66,19 @@ router.get("/admin/categories/edit/:id", (req, res) => {
     })
 })
 
+//atualizando dados no sequelize
+router.post("/categories/update", (req, res) => {
+    var id =req.body.id;
+    var title = req.body.title;
+
+    Category.update({title: title, slug: slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/categories");
+    })
+})
+
 
 module.exports = router;
